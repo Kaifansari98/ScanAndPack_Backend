31c6f7785df3d9b32e9699da949ed9a315c7443a
Kaifansari98 <kaifansari54540@gmail.com>
2025-08-05 12:56:13 +0530

    Also passed the Total of weight*qty in the getProjectById API

--- src/controllers/projectControllers/project.controller.ts
+++ src/controllers/projectControllers/project.controller.ts
@@ -71,7 +71,7 @@ export const getProjectById = async (req: Request, res: Response) => {
     const total_packed = project.details.reduce((sum, d) => sum + d.total_packed, 0);
     const total_unpacked = project.details.reduce((sum, d) => sum + d.total_unpacked, 0);
     const total_items_count = project.items.length;
-    const total_weight = project.items.reduce((sum, item) => sum + (item.weight || 0), 0);
+    const total_weight = project.items.reduce((sum, item) => sum + (item.weight * item.qty || 0), 0);
 
     // Send combined response
     res.json({
