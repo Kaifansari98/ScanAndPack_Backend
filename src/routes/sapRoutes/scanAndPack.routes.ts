cb9dfd6071243c0562da21c611d5845277ff4ca3
root <root@srv954177.hstgr.cloud>
2025-08-12 14:35:42 +0000

    commit from server

--- src/routes/sapRoutes/scanAndPack.routes.ts
+++ src/routes/sapRoutes/scanAndPack.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { addScanAndPackItem, getScanAndPackItemsByFields, deleteScanAndPackItem } from '../../controllers/sapControllers/scanAndPack.controller';
-import { createScanItem } from '../../controllers/sapControllers/scanItem.controller';
 
 const router = Router();
 
-router.post('/', createScanItem);
 router.post('/scan-and-pack/add', addScanAndPackItem);
 router.delete('/scan-and-pack/delete/:id', deleteScanAndPackItem);
 router.post('/by-fields', getScanAndPackItemsByFields);
