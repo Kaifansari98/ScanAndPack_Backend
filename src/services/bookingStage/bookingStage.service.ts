cfcf5a699b072ade5c4f837eb3fb82716abd9d39
CrestWebStudios <154508851+Kaifansari98@users.noreply.github.com>
2026-02-09 11:22:29 +0530

    test issue fix

--- src/services/bookingStage/bookingStage.service.ts
+++ src/services/bookingStage/bookingStage.service.ts
@@ -47,7 +47,10 @@ export class BookingStageService {
           tech_check_completed_at: true,
           is_order_login_completed: true,
           order_login_completed_at: true,
-          productStructure: { select: { id: true, type: true } },
+          // productStructure: { select: { id: true, type: true } },
+        },
+        include: {
+          productStructure: true,
         },
         orderBy: [{ product_structure_id: Prisma.SortOrder.asc }, { quantity_index: Prisma.SortOrder.asc }],
       },
