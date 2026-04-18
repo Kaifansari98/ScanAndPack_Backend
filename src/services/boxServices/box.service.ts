1460f2a74e11f5a08af316bb6655ebe76bb36f52
Daniyal Shaikh <daniyal.shaikh@confidevtech.com>
2026-04-18 16:45:56 +0530

    true added

--- src/services/boxServices/box.service.ts
+++ src/services/boxServices/box.service.ts
@@ -709,7 +709,7 @@ export const generateBoxPdfService = async (
 
 export async function generatePdf(html: string, filePath: string) {
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless:true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"], // required on VPS
   });
 
