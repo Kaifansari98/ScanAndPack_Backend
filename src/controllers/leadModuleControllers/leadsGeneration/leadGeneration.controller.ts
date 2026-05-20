e68ca7dbbd5de16f8bdbd7b4afe7dbb0c420857f
arfaj.sheru <arfatsheru74@gmail.com>
2026-05-20 12:53:54 +0530

    feat(leads): support task_type followup to fetch all users without role restriction

--- src/controllers/leadModuleControllers/leadsGeneration/leadGeneration.controller.ts
+++ src/controllers/leadModuleControllers/leadsGeneration/leadGeneration.controller.ts
@@ -1623,6 +1623,9 @@ export class LeadController {
       const requiredPrivilegeCode = req.query.required_privilege_code
         ? String(req.query.required_privilege_code)
         : undefined;
+      const taskType = req.query.task_type
+        ? String(req.query.task_type)
+        : undefined;
 
       // Validate vendorId
       if (isNaN(vendorId) || vendorId <= 0) {
@@ -1649,6 +1652,7 @@ export class LeadController {
         {
           assigneeUserType,
           requiredPrivilegeCode,
+          taskType,
         },
       );
 
