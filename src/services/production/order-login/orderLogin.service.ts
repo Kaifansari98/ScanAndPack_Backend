d14f37978f0b9ebeaad98bf45e7c350ec7f70042
arfaj.sheru <arfatsheru74@gmail.com>
2026-05-20 12:55:22 +0530

    support task_type followup to fetch all users without role restriction

--- src/services/production/order-login/orderLogin.service.ts
+++ src/services/production/order-login/orderLogin.service.ts
@@ -327,9 +327,9 @@ export async function triggerOrderLoginCompletionNotification(
       type: NotificationType.LEAD_ACTION,
       title: "Order Login Completed",
       ...(instanceId
-        ? { metadata: { path: ["instance_id"], equals: instanceId } }
+        ? { message: { startsWith: `${instanceCode} - ` } }
         : {}),
-    },
+    },  
   });
 
   if (alreadySent) {
