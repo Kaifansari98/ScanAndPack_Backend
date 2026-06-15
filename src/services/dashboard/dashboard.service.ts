a1c5832deb25223c43e36c93ea4d05eee49ba5f5
CrestWebStudios <154508851+Kaifansari98@users.noreply.github.com>
2026-06-15 13:22:37 +0530

    done

--- src/services/dashboard/dashboard.service.ts
+++ src/services/dashboard/dashboard.service.ts
@@ -648,6 +648,7 @@ export class DashboardService {
           vendor_id,
           status_id,
           is_deleted: false,
+          ...(typeTag === "Type 1" ? { is_draft: false } : {}),
           ...activityStatusFilter,
           ...userFilter,
         },
